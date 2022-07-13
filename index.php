5812a21dadb25c2c8925951a02cac6c6b5f97d17
Anto231996 <tony2396@hotmail.it>
2022-07-13 16:41:05 +0200

    commit

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
+<body>
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -11,15 +22,25 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$flights = "SELECT id, code_departure, code_arrival, price FROM flights";
-$airports = "SELECT id, code_departure, code_arrival, price FROM airports";
-$result = $conn->query($flights);
+/* $flights = "SELECT id, code_departure, code_arrival, price FROM flights"; */
+/* $all = "SELECT ALL FROM airports INNER JOIN flights"; */
+$all = "SELECT * FROM airports AS a, flights AS f WHERE f.code_departure = a.code OR f.code_arrival = a.code ORDER BY `f`.`id`";
+
+$result = $conn->query($all);
 
 if ($result->num_rows > 0) {
-  // output data of each row
+    echo "<table><tr><th>Nome</th><th>Codice</th><th>Latitudine</th><th>Longitudine</th><th>ID</th><th>Codice partenza</th><th>Codice arrivo</th><th>Prezzo</th></tr>";
+    // output data of each row
     while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Codice: " . $row["code_departure"]. " " . $row["code_arrival"]. " - Prezzo: " . $row["price"]. "<br>";
+        echo "<tr><td>" . $row["name"]. "</td><td>" . $row["code"]. "</td><td>" . $row["lat"]. "</td><td>" . $row["lng"]. "</td><td>" . $row["id"]. "</td><td>" . $row["code_departure"]. "</td><td>" . $row["code_arrival"]. "</td><td>" . $row["price"]. "</td></tr>";
     }
-} else { echo "0 risultati"; }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
 $conn->close();
 ?>
+
+</body>
+</html>
